f336e1f075cb84825e9d126f43f297a2c6f16030
Leroy0824 <kinged.1234567890@gmail.com>
2026-04-30 15:44:57 +0100

    Update Figure 3.R

--- Figure 3.R
+++ Figure 3.R
@@ -3,6 +3,6 @@ y<-pnorm((2.32625-1.95996)/x)
 plot(x,y,type="l",xlab="Replication noise",ylab="Reproducible probability")
 abline(h=0.5,col="Red")
 legend("topright",
-       legend=c("s=1","p=0.5"),
+       legend=c("p=0.5","s=1"),
        col=c("red","black"),
        lty=c(1,1))
